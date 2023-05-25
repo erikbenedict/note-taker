b19e04b95dffbfb2613434feda9d7ecba11a6f62
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-05-25 08:16:04 -0400

    chore: added comments to 'server.js'

--- server.js
+++ server.js
@@ -4,19 +4,23 @@ const app = express();
 const path = require("path");
 const apiRoutes = require("./routes/apiRoutes");
 
+// * Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
 
+// * Serve the index.html file at the root URL
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// * Serve the notes.html file at the /notes URL
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+// * Start the server and listen on the specified por
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
