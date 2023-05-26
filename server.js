5530e7a8f974bc612bc7a5802820a0aea659dbc8
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-05-25 22:48:33 -0400

    refactor: changed all double quotes to single quotes

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require("express");
+const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const path = require("path");
-const apiRoutes = require("./routes/apiRoutes");
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
 
 // * Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/api", apiRoutes);
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
 
 // * Serve the index.html file at the root URL
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // * Serve the notes.html file at the /notes URL
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // * Start the server and listen on the specified por
